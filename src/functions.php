75c85db9fee47c6f5dab810a609ac5df4f106fea
coder <theveterancoder@gmail.com>
2020-07-20 11:24:36 -0700

    add status code

--- src/functions.php
+++ src/functions.php
@@ -303,3 +303,18 @@ if(!function_exists('getLatestId')){
         return $id;
     }
 }
+
+if(!function_exists('getStatusCode')){
+    function getStatusCode($url){
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+        curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_TIMEOUT,10);
+        $output = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return  $httpcode;
+    }
+}
